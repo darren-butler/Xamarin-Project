8f27f68ce0e0aa037ad1cb8f4596e5426a78d9e4
Darren <Darren@DESKTOP-ODU73U1>
2018-12-12 19:07:25 +0000

    0.3_CourseSetup

--- IAD_Project/IAD_Project/Views/CourseOverviewPage.xaml.cs
+++ IAD_Project/IAD_Project/Views/CourseOverviewPage.xaml.cs
@@ -8,6 +8,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 using IAD_Project.Models;
+using IAD_Project.Views;
 using Newtonsoft.Json;
 using System.IO;
 
@@ -17,15 +18,37 @@ namespace IAD_Project.Views
 	public partial class CourseOverviewPage : ContentPage
 	{
         Course course = new Course();
+        int YearNum;
 
         public CourseOverviewPage ()
 		{
 			InitializeComponent ();
             course = Utility.DeserializeCourse();
+            SetupYearButtons();
 
             /*DEBUGGING
             lblCourseName.Text = course.Name;
             lblCurrentYear.Text = (course.Years[0].YearNumber + 1).ToString(); */
         }
+
+        private void SetupYearButtons()
+        {
+
+            for (int i = 0; i < course.NumOfYears; i++)
+            {
+                var btn = new Button();
+
+                btn.Text = "Year " + (course.Years[i].YearNumber + 1).ToString() + " Average: " + (course.Years[i].GradeAverage).ToString();
+
+                layout.Children.Add(btn);
+
+            }// for
+        }
+
+        private async void btnYear_Clicked()
+        {
+            await Navigation.PushAsync(new YearPage());
+        }
+
     }
 }
